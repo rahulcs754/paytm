770fd09057ee3021a8c29063385d4f2d5eeab4e7
rahulcs754 <rahul.shukla754@gmail.com>
2024-01-27 17:31:23 +0530

    setting for run project

--- frontend/src/Router/index.jsx
+++ frontend/src/Router/index.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, SignUp, SignIn, DashBoard, SendMoney } from "../Components";
+import { SignUp, SignIn, DashBoard, SendMoney } from "../Components";
 
 const Routespaths = () => {
   return (
     <Routes>
-      <Route exact path="/" element={<HomePage />}></Route>
-      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/" element={<SignUp />}></Route>
       <Route path="/signin" element={<SignIn />}></Route>
       <Route path="/dashboard" element={<DashBoard />}></Route>
       <Route path="/send" element={<SendMoney />}></Route>
